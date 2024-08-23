389645344c06e65736eebb50e126c5f9d37e1532
Lineax17 <144595200+Lineax17@users.noreply.github.com>
2024-08-23 12:05:45 +0200

    Adding test.json and TestJSON.vue for testing purposes

---
+++ src/components/TestJSON.vue
@@ -0,0 +1,29 @@
+<script>
+import { ref } from 'vue'
+import stratagemData from '../../test.json'
+
+const stratagems = ref(stratagemData.subsection["General Stratagems"].stratagems)
+
+export default {
+  name: 'TestJSON',
+  setup() {
+    return {
+      stratagems
+    }
+  }
+}
+</script>
+
+<template>
+<div>
+  <ul>
+    <li v-for="stratagem in stratagems" :key="stratagem.name">
+      {{ stratagem.name }}
+    </li>
+  </ul>
+</div>
+</template>
+
+<style scoped>
+
+</style>
