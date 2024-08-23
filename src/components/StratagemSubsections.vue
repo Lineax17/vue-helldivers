2c8bc3db20148de65d20d2e9e25443887757ac6e
Lineax17 <144595200+Lineax17@users.noreply.github.com>
2024-08-23 12:05:15 +0200

    Adding StratagemSubsections.vue

---
+++ src/components/StratagemSubsections.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue'
+import stratagemData from '../../stratagems.json'
+
+const subsections = ref(Object.keys(stratagemData.subsections))
+</script>
+
+<template>
+  <div>
+    <ul>
+      <li v-for="subsection in subsections" :key="subsection">
+        <h3>{{ subsection }}</h3>
+        <ul>
+          <li v-for="stratagem in stratagemData.subsections[subsection].stratagems" :key="stratagem.name">
+            {{ stratagem.name }}
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
